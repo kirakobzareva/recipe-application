1ee971e98324841911bcbaf3e28be972c60c6f71
Kira Kobzareva <kirakobzareva@gmail.com>
2022-07-11 13:41:54 +0200

    add changing in closing dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]'
+  selector: '[appDropdown]',
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
+  
+  constructor(private elRef: ElementRef) { }
 
-  constructor() { }
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
- }
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+
+  // constructor() { }
+  //   @HostListener('click') toggleOpen() {
+  //     this.isOpen = !this.isOpen;
+  //  }
 }
